eca08ad37b23c5b6e0b2d2016018ab5a5e44278f
Гилязова Альбина <giliazova@mail.ru>
2024-02-28 19:08:30 +0300

    Fix on tests.

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-from parts import MaleFemaleGenerator
+from parts import InputDataSource, GeneratorDataSource, FileDataSource
 
 if __name__ == '__main__':
-    fk = MaleFemaleGenerator()
-    for line in fk.generate(10):
-        print(line)
+    # fk = MaleFemaleGenerator()
+    # for line in fk.generate(10):
+    #     print(line)
+
+    # ids = InputDataSource()
+    # for line in ids.get_data():
+    #     print(line)
+
+    # ids = GeneratorDataSource()
+    # for line in ids.get_data():
+    #     print(line)
+
+    ids = FileDataSource()
+    for line in ids.get_data():
+        print(line)   
+        

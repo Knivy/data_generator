0beb246c1e631011bbbdc743b14ec82679b10adc
Гилязова Альбина <giliazova@mail.ru>
2024-03-08 03:56:28 +0300

    Refactor data_source

--- modules/data_source.py
+++ modules/data_source.py
@@ -12,7 +12,11 @@ class BaseDataSource:
 
     default_num_lines: int = 500_000  # Сколько нужно строк.
 
-    def get_data(self, num_lines: int = default_num_lines):
+    def __init__(self, num_lines: int = default_num_lines):
+        """Получает число строк."""
+        self.num_lines = num_lines
+
+    def get_data(self):
         """Получает данные и отдает построчно."""
         raise NotImplementedError('Этот метод должен быть переопределён.')
 
@@ -20,12 +24,12 @@ class BaseDataSource:
 class InputDataSource(BaseDataSource):
     """Данные от ввода пользователя."""
 
-    def get_data(self, num_lines: int = BaseDataSource.default_num_lines):
+    def get_data(self):
         """Получает данные от пользователя и отдает построчно."""
 
         print('Введите данные построчно через пробел.',
               'Enter - окончание ввода.')
-        for _ in range(num_lines):
+        for _ in range(self.num_lines):
             while True:
                 line = input('Введите строку: ')
                 if line:
@@ -38,11 +42,13 @@ class GeneratorDataSource(BaseDataSource):
 
     default_generator_type = 'Mimesis'
 
-    def __init__(self, language: str = BaseGenerator.default_language,
+    def __init__(self,  num_lines: int = BaseDataSource.default_num_lines,
+                 language: str = BaseGenerator.default_language,
                  format: str = BaseGenerator.default_format,
                  header: bool = BaseGenerator.header,
                  generator_type: str = default_generator_type):
 
+        super().__init__(num_lines)
         self.header = header
         if generator_type.lower() == 'faker':
             self.generator: BaseGenerator = MaleFemaleFakerGenerator(language,
@@ -50,9 +56,9 @@ class GeneratorDataSource(BaseDataSource):
         else:
             self.generator = MaleFemaleMimesisGenerator(language, format)
 
-    def get_data(self, num_lines: int = BaseDataSource.default_num_lines):
+    def get_data(self):
         """Получает данные от генератора."""
-        for line in self.generator.generate(num_lines, self.header):
+        for line in self.generator.generate(self.num_lines, self.header):
             yield line
 
 
@@ -61,8 +67,10 @@ class FileDataSource(BaseDataSource):
 
     file_formats: tuple
 
-    def __init__(self, file_path: str):
-        """Получает путь к файлу."""
+    def __init__(self, file_path: str,
+                 num_lines: int = BaseDataSource.default_num_lines):
+        """Получает настройки."""
+        super().__init__(num_lines)
         self.file_path = file_path
 
 
@@ -72,19 +80,21 @@ class CsvDataSource(FileDataSource):
     file_formats = ('csv', 'txt')
     default_separator: str = '\t'
 
-    def __init__(self, file_path: str, separator: str = default_separator):
+    def __init__(self, file_path: str,
+                 num_lines: int = BaseDataSource.default_num_lines,
+                 separator: str = default_separator):
         """Получает настройки."""
-        super().__init__(file_path)
+        super().__init__(file_path, num_lines)
         self.separator = separator
 
-    def get_data(self, num_lines: int = BaseDataSource.default_num_lines):
+    def get_data(self):
         """Получает данные из файла txt или csv."""
 
         with open(self.file_path, 'r', encoding='utf-8') as f:
             line_count = 0
             for line in f.readlines():
                 line_count += 1
-                if line_count > num_lines:
+                if line_count > self.num_lines:
                     raise StopIteration
                 line = line.strip()
                 if line:
@@ -96,12 +106,12 @@ class XlsxDataSource(FileDataSource):
 
     file_formats = ('xlsx',)
 
-    def get_data(self, num_lines: int = BaseDataSource.default_num_lines):
+    def get_data(self):
         """Получает данные из файла xlsx."""
         workbook = openpyxl.load_workbook(self.file_path)
         worksheet = workbook.active
         length = worksheet.max_row
-        num_lines = min(length, num_lines)
+        num_lines = min(length, self.num_lines)
         for row in range(num_lines):
             line = []
             for col in worksheet.iter_cols(1, worksheet.max_column):
@@ -111,24 +121,25 @@ class XlsxDataSource(FileDataSource):
 
 class XlsDataSource(FileDataSource):
     """
-    Данные из первого листа xls-файла.
+    Данные из xls-файла.
     """
 
     file_formats = ('xls',)
     default_sheet_number = 0
 
-    def __init__(self, file_path: str,
+    def __init__(self, file_path: str,,
+                 num_lines: int = BaseDataSource.default_num_lines,
                  sheet_number: int = default_sheet_number):
         """Получает настройки."""
-        super().__init__(file_path)
+        super().__init__(file_path, num_lines)
         self.sheet_number = sheet_number
 
-    def get_data(self, num_lines: int = BaseDataSource.default_num_lines):
+    def get_data(self):
         """Получает данные из файла xls."""
         workbook = xlrd.open_workbook(self.file_path)
         worksheet = workbook.sheet_by_index(self.sheet_number)
         length = worksheet.nrows
-        num_lines = min(length, num_lines)
+        num_lines = min(length, self.num_lines)
         for row in range(num_lines):
             line = []
             for col in range(worksheet.ncols):
